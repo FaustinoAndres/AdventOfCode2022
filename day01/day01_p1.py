acc9df9bf0d4ca2c54fe3ce75fdd951e2a470c22
Faustino Correa <faustino.correa.m@gmail.com>
2022-12-02 16:59:07 -0300

    day 01 part 1 OK

---
+++ day01/day01_p1.py
@@ -0,0 +1,27 @@
+
+def puzzle():
+
+    with open('input.txt', 'r') as file:
+        lines = file.readlines()
+
+    return lines
+
+def main():
+
+    lines = list(map(lambda line: line.rstrip('\n'), puzzle()))
+    elves = {}
+    elf = 0
+    calories = 0
+    for line in lines:
+        if line != '':
+            calories += int(line)
+        else:
+            elf += 1
+            elves[elf] = calories
+            calories = 0
+
+    print(max(list(elves.values())))
+
+if __name__ == '__main__':
+
+    main()
